48ab2f005f45308d90731c4e6acf426bfc04752b
Durendal <durendal@durendals-domain.com>
2022-12-01 12:18:40 -0600

    Owlpdates

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -1,4 +1,3 @@
 data = [j.split("\n") for j in [i for i in open('input1.txt', 'r').read().split("\n\n")]]
-del data[-1][-1]
 print(sum(sorted([sum([int(j) for j in data[i] if j != '']) for i in range(len(data))])[-3:]))
 
