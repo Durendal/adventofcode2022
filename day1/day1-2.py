a2db164c438889ecac0b2e0a5cfcc38273fe54e2
Durendal <durendal@durendals-domain.com>
2022-12-01 12:17:42 -0600

    Updates

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -1,4 +1,4 @@
 data = [j.split("\n") for j in [i for i in open('input1.txt', 'r').read().split("\n\n")]]
 del data[-1][-1]
-print(sorted([sum([int(j) for j in data[i] if j != '']) for i in range(len(data))])[::-3])
+print(sum(sorted([sum([int(j) for j in data[i] if j != '']) for i in range(len(data))])[-3:]))
 
