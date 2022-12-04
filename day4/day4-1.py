720933925381867f95abf9c6bdedfd3e95a071c4
Durendal <durendal@durendals-domain.com>
2022-12-04 15:30:03 -0600

    Removed redundant casting to list

--- day4/day4-1.py
+++ day4/day4-1.py
@@ -2,8 +2,8 @@ data = [i.strip() for i in open('input.txt', 'r').readlines() if i != '\n']
 count = 0
 for row in data:
   line = row.split(',')
-  l1, r1 = list(map(int, line[0].split("-")))
-  l2, r2 = list(map(int, line[1].split("-")))
+  l1, r1 = map(int, line[0].split("-"))
+  l2, r2 = map(int, line[1].split("-"))
   if (l1 >= l2 and r1 <= r2) or (l1 <= l2 and r1 >= r2):
     count += 1
 print(count)
--- day4/day4-2.py
+++ day4/day4-2.py
@@ -2,8 +2,8 @@ data = [i.strip() for i in open('input.txt', 'r').readlines() if i != '\n']
 count = 0
 for row in data:
   line = row.split(',')
-  l1, r1 = list(map(int, line[0].split("-")))
-  l2, r2 = list(map(int, line[1].split("-")))
+  l1, r1 = map(int, line[0].split("-"))
+  l2, r2 = map(int, line[1].split("-"))
   s1 = set(range(l1, r1+1))
   s2 = set(range(l2, r2+1))
   if s1.intersection(s2): 
