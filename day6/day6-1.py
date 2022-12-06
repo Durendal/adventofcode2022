85d117137497bb347cd92f897e695680b87fb49d
Durendal <durendal@durendals-domain.com>
2022-12-05 23:57:12 -0600

    Fewer lines bitch

--- day6/day6-1.py
+++ day6/day6-1.py
@@ -1,5 +1,3 @@
 data = open('input.txt', 'r').read()
-for i in range(len(data)-4):
-  if len(set(data[i:i+4])) == 4:
-    print(i+4)
-    break
+a = [i for i in range(len(data)-4) if len(set(data[i:i+4])) == 4]
+print(a[0]+4)
